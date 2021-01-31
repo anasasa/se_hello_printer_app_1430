137bd9b8d3e4488aa465e84233c1bd147a052974
anasasa <a.kuchlewski@interia.pl>
2021-01-31 10:32:57 +0100

    new formatting

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -3,8 +3,9 @@ PLAIN = "plain"
 PLAIN_UP = "plain_uppercase"
 PLAIN_LO = "plain_lowercase"
 JSON = "json"
+HIDD = "hidden_html"
 
-SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON]
+SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON, HIDD]
 
 
 def get_formatted(msg, imie, format):
@@ -17,6 +18,8 @@ def get_formatted(msg, imie, format):
         result = plain_text_lower_case(msg, imie)
     elif format == JSON:
         result = format_to_json(msg, imie)
+    elif result == HIDD:
+        result = hidden_html(mgs, imie)
     return result
 
 
@@ -24,14 +27,16 @@ def format_to_json(msg, imie):
     return ('{ "imie":"' + imie + '", "msg":"' +
             msg + '"}')
 
-
 def plain_text(msg, imie):
     return imie + ' ' + msg
-
-
+    
 def plain_text_upper_case(msg, imie):
     return plain_text(msg.upper(), imie.upper())
 
-
 def plain_text_lower_case(msg, imie):
     return plain_text(msg.lower(), imie.lower())
+    
+def hidden_html(msg, imie):
+    return imie + ' ' + msg
+    
+    #return '<!doctype html><html><body><p>Hello World!</p>' + imie + ' pozdrawia ;)</body></html>'  
